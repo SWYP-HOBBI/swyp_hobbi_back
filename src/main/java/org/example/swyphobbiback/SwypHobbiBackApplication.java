8cf93daed3a4a2a15da9e2185fe2ea0c74b811de
ChoiSeoWoong <chldbfkr456@naver.com>
2025-04-19 16:18:07 +0900

    Initial Commit

---
+++ src/main/java/org/example/swyphobbiback/SwypHobbiBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.swyphobbiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwypHobbiBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwypHobbiBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/swyphobbiback/SwypHobbiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.swyphobbiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwypHobbiBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
