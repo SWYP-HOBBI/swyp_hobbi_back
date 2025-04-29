668aadd2475d1ca55c84cce0ceee685d5db96324
Woongineering <chldbfkr456@naver.com>
2025-04-29 22:51:18 +0900

    feat : 태그명으로 in쿼리 로직 추가

--- src/main/java/swyp/hobbi/swyphobbiback/hobbytag/repository/HobbyTagRepository.java
+++ src/main/java/swyp/hobbi/swyphobbiback/hobbytag/repository/HobbyTagRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import swyp.hobbi.swyphobbiback.hobbytag.domain.HobbyTag;
 
+import java.util.List;
+
 @Repository
 public interface HobbyTagRepository extends JpaRepository<HobbyTag,Long> {
+    List<HobbyTag> findAllByHobbyTagNameIn(List<String> hobbyTagNames);
 }
