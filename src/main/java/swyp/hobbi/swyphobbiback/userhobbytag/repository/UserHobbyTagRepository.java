d30d22e6a2db71cc155846e2a4cc6093a227f415
Woongineering <chldbfkr456@naver.com>
2025-04-30 00:34:44 +0900

    refactor : repository jpa 네이밍 룰에 맞게 수정

--- src/main/java/swyp/hobbi/swyphobbiback/userhobbytag/repository/UserHobbyTagRepository.java
+++ src/main/java/swyp/hobbi/swyphobbiback/userhobbytag/repository/UserHobbyTagRepository.java
@@ -1,10 +1,12 @@
 package swyp.hobbi.swyphobbiback.userhobbytag.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import swyp.hobbi.swyphobbiback.userhobbytag.domain.UserHobbyTag;
 
 import java.util.List;
 
+@Repository
 public interface UserHobbyTagRepository extends JpaRepository<UserHobbyTag, Long> {
-    List<UserHobbyTag> findAllByUserId(Long userId);
+    List<UserHobbyTag> findAllByUser_UserId(Long userId);
 }
