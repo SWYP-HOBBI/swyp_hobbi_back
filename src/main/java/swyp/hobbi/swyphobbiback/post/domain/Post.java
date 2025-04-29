d0060cd441d7f72b3ae02c4cf0e4ba0a5e611999
Woongineering <chldbfkr456@naver.com>
2025-04-29 23:07:25 +0900

    feat : Post entity 생성

---
+++ src/main/java/swyp/hobbi/swyphobbiback/post/domain/Post.java
@@ -0,0 +1,58 @@
+package swyp.hobbi.swyphobbiback.post.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import swyp.hobbi.swyphobbiback.post_hobbytag.domain.PostHobbyTag;
+import swyp.hobbi.swyphobbiback.post_image.domain.PostImage;
+import swyp.hobbi.swyphobbiback.user.domain.User;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "post")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+@ToString
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String postTitle;
+
+    private String postContent;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PostHobbyTag> postHobbyTags = new ArrayList<>();
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PostImage> postImages = new ArrayList<>();
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public void update(String postTitle, String postContent, List<PostHobbyTag> hobbyTags, List<PostImage> images) {
+        this.postTitle = postTitle;
+        this.postContent = postContent;
+        this.postHobbyTags = hobbyTags;
+        this.postImages = images;
+        updatedAt = LocalDateTime.now();
+    }
+}
