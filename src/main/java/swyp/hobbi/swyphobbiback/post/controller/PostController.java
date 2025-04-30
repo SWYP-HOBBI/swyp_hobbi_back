496cf2f3b3a25e080ea659ac1516a14a95c7224e
Woongineering <chldbfkr456@naver.com>
2025-05-01 02:52:05 +0900

    feat : 게시글 작성 & ObjectStorage 업로드 메서드 추가

--- src/main/java/swyp/hobbi/swyphobbiback/post/controller/PostController.java
+++ src/main/java/swyp/hobbi/swyphobbiback/post/controller/PostController.java
@@ -1,12 +1,33 @@
 package swyp.hobbi.swyphobbiback.post.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import swyp.hobbi.swyphobbiback.common.security.CustomUserDetails;
+import swyp.hobbi.swyphobbiback.post.dto.PostCreateRequest;
+import swyp.hobbi.swyphobbiback.post.dto.PostResponse;
+import swyp.hobbi.swyphobbiback.post.service.PostService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/post")
+@Slf4j
 public class PostController {
+    private final PostService postService;
 
+    @PostMapping
+    public ResponseEntity<PostResponse> createPost(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestPart("request") PostCreateRequest request,
+            @RequestPart(value = "imageFiles", required = false) List<MultipartFile> imageFiles
+    ) {
+        PostResponse response = postService.create(userDetails, request, imageFiles);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
