4d38d1c6def0cfd30b5900d8a2c96025b4d67ede
asdf1019 <jiwon3157@naver.com>
2025-04-30 20:24:16 +0900

    refactor: JWT Properties 추가, TokenProvider 수정

---
+++ src/main/java/swyp/hobbi/swyphobbiback/common/security/JwtProperties.java
@@ -0,0 +1,18 @@
+package swyp.hobbi.swyphobbiback.common.security;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+
+    private String secretKey;
+    private long accessTokenExpireTime;
+    private long refreshTokenExpireTime;
+
+}
