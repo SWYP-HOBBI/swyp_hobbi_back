384a1b0c355fea17c06a37ea43ca3b5d4dd32ab0
Woongineering <chldbfkr456@naver.com>
2025-05-05 01:59:41 +0900

    feat : Comment 엔티티 생성

---
+++ src/main/java/swyp/hobbi/swyphobbiback/comment/domain/Comment.java
@@ -0,0 +1,51 @@
+package swyp.hobbi.swyphobbiback.comment.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import swyp.hobbi.swyphobbiback.post.domain.Post;
+import swyp.hobbi.swyphobbiback.user.domain.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "post_comment")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    private Long parentCommentId;
+    private String commentContent;
+    private Boolean deleted;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public boolean isRoot() {
+        return parentCommentId == null;
+    }
+
+    public void setDeletedTrue() {
+        deleted = true;
+    }
+}
