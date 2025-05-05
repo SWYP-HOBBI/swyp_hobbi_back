f6c6c9b274923e5edbf835e92bd34e6b692acd8e
ChoiSeoWoong <chldbfkr456@naver.com>
2025-05-05 21:39:40 +0900

    feat : update 로직 추가

--- src/main/java/swyp/hobbi/swyphobbiback/comment/service/CommentService.java
+++ src/main/java/swyp/hobbi/swyphobbiback/comment/service/CommentService.java
@@ -1,11 +1,12 @@
 package swyp.hobbi.swyphobbiback.comment.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import swyp.hobbi.swyphobbiback.comment.domain.Comment;
-import swyp.hobbi.swyphobbiback.comment.dto.CommentCreateRequest;
+import swyp.hobbi.swyphobbiback.comment.dto.CommentRequest;
 import swyp.hobbi.swyphobbiback.comment.dto.CommentResponse;
 import swyp.hobbi.swyphobbiback.comment.repository.CommentRepository;
 import swyp.hobbi.swyphobbiback.common.error.ErrorCode;
@@ -22,13 +23,14 @@ import static java.util.function.Predicate.*;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CommentService {
     private final CommentRepository commentRepository;
     private final UserRepository userRepository;
     private final PostRepository postRepository;
 
     @Transactional
-    public CommentResponse create(CustomUserDetails userDetails, CommentCreateRequest request) {
+    public CommentResponse create(CustomUserDetails userDetails, CommentRequest request) {
         if(userDetails == null || userDetails.getUsername() == null) {
             throw new AccessDeniedException(ErrorCode.FORBIDDEN.getMessage());
         }
@@ -43,14 +45,14 @@ public class CommentService {
                 .user(user)
                 .post(post)
                 .parentCommentId(parentComment == null ? null : parentComment.getCommentId())
-                .commentContent(request.getComment())
+                .commentContent(request.getContent())
                 .deleted(false)
                 .build();
 
         return CommentResponse.from(commentRepository.save(comment));
     }
 
-    private Comment findParentComment(CommentCreateRequest request) {
+    private Comment findParentComment(CommentRequest request) {
         Long parentCommentId = request.getParentCommentId();
 
         if(parentCommentId == null) {
@@ -61,5 +63,20 @@ public class CommentService {
                 .filter(not(Comment::getDeleted))
                 .filter(Comment::isRoot)
                 .orElseThrow(CommentNotFoundException::new);
+    }
+
+    @Transactional
+    public CommentResponse update(CustomUserDetails userDetails, Long commentId, CommentRequest request) {
+        if(!userDetails.getUserId().equals(request.getUserId())) {
+            throw new AccessDeniedException(ErrorCode.FORBIDDEN.getMessage());
+        }
+
+        Comment comment = commentRepository.findById(commentId)
+                .filter(not(Comment::getDeleted))
+                .orElseThrow(CommentNotFoundException::new);
+
+        comment.update(request.getContent());
+
+        return CommentResponse.from(comment);
     }
 }
