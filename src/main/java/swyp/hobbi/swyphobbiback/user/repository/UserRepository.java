0220593f573b569a6195985dd87d68a9c97683e0
Woongineering <chldbfkr456@naver.com>
2025-05-14 01:30:11 +0900

    feat : userId들로 닉네임들을 찾는 쿼리 추가

--- src/main/java/swyp/hobbi/swyphobbiback/user/repository/UserRepository.java
+++ src/main/java/swyp/hobbi/swyphobbiback/user/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package swyp.hobbi.swyphobbiback.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import swyp.hobbi.swyphobbiback.user.domain.User;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,4 +14,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email); //이메일 중복 검사
 
     Optional<User> findByEmail(String email);
+
+    @Query(value = """
+        SELECT u.userId, u.nickname
+        FROM User u
+        WHERE u.userId IN(:userIds)
+        """)
+    Map<Long, String> findNicknamesByIds(@Param("userIds") List<Long> userIds);
 }
