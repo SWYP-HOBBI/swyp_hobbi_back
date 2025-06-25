251321dae2590be9dc3c8e5f08e129a760902ab2
Woongineering <chldbfkr456@naver.com>
2025-06-25 22:58:26 +0900

    refactor : ncp objectStorage -> aws s3로 마이그레이션

--- src/main/java/swyp/hobbi/swyphobbiback/common/config/S3Config.java
+++ src/main/java/swyp/hobbi/swyphobbiback/common/config/S3Config.java
@@ -2,8 +2,7 @@ package swyp.hobbi.swyphobbiback.common.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,23 +10,22 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
-    @Value("${ncp.storage.region}")
-    private String region;
-
-    @Value("${ncp.storage.endpoint}")
-    private String endpoint;
-
-    @Value("${ncp.storage.accessKey}")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
-    @Value("${ncp.storage.secretKey}")
+    @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
     @Bean
-    public AmazonS3Client objectStorageClient() {
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+    public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(
+                        new BasicAWSCredentials(accessKey, secretKey)
+                ))
                 .build();
     }
 }
--- src/main/java/swyp/hobbi/swyphobbiback/post_image/service/PostImageService.java
+++ src/main/java/swyp/hobbi/swyphobbiback/post_image/service/PostImageService.java
@@ -1,9 +1,7 @@
 package swyp.hobbi.swyphobbiback.post_image.service;
 
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,23 +9,19 @@ import swyp.hobbi.swyphobbiback.post.domain.Post;
 import swyp.hobbi.swyphobbiback.post_image.domain.PostImage;
 import swyp.hobbi.swyphobbiback.post_image.repository.PostImageRepository;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class PostImageService {
-    private static final String BUCKET_NAME = "hobbi-img";
+    private static final String BUCKET_NAME = "hobbi-dev";
     private static final String PREFIX_FILE_NAME = "post";
     private static final String FILE_FORMAT = "%s%s%s%s";
     private static final String FILE_SEPARATOR = "-";
     private static final String DIRECTORY_SEPARATOR = "/";
-    private static final String PREFIX_IMAGE_URL = "https://kr.object.ncloudstorage.com/";
+    private static final String PREFIX_IMAGE_URL = "https://hobbi-dev.s3.ap-northeast-2.amazonaws.com/";
 
-    private final AmazonS3Client objectStorageClient;
+    private final AmazonS3 amazonS3;
 
     private final PostImageRepository postImageRepository;
 
@@ -43,7 +37,7 @@ public class PostImageService {
 
     public void deletePostImage(String imageUrl) {
         DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(BUCKET_NAME, imageUrl);
-        objectStorageClient.deleteObject(deleteObjectRequest);
+        amazonS3.deleteObject(deleteObjectRequest);
     }
 
     public String fileNameFormatter(MultipartFile file) {
